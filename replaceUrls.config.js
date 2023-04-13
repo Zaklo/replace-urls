f0706bd053cef71e5245403ed0c7a200a5491860
Antoine Lozach <lozachantoine@gmail.com>
2023-04-13 16:19:58 +0200

    1.0.1

--- replaceUrls.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    htmlPath: 'index.html',
-    mediaSrcSelector: 'img[src^="https://res.cloudinary.com"], video[src^="https://res.cloudinary.com"]',
-    mediaDir: 'medias/'
-};
